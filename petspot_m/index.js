206619c91eb068fcb8c1b39280d7b07142bcb942
LUCAS DE SOUSA SANTOS <lucas.ssantos189@senacsp.edu.br>
2025-02-11 21:20:39 -0300

    first commit

---
+++ petspot_m/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const router = require('./src/routes/routes')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(router)
+
+app.listen(process.env.PORT, () => {
+    console.log(`Aplicação esta rodando na porta ${process.env.PORT}`)
+})
+
+app.get('/', (request, response) => {
+    response.send('Olá mundo!')
+})
